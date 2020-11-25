f15ac06f55c9398c05013251a2d4260dd088603e
Shania Mitra <smitra@wn-lot-008.nikhef.nl>
2020-11-25 10:49:20 +0100

    copy files for compressed image

---
+++ Paul_run_script_copy.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python3
+# Import Class from utils.py & net.py file
+from utils import DataPreprocess, Parameters, Parameters_reduced
+from net_copy import SNDNet, MyDataset, digitize_signal
+# usful module 
+import torch
+from matplotlib import pylab as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import time
+from tqdm import tqdm
+from IPython import display
+import os
+import gc # Gabage collector interface (to debug stuff)
+#import tracemalloc
+# Test to see if cuda is available or not + listed the CUDA devices that are available
+try:
+    assert(torch.cuda.is_available())
+except:
+    raise Exception("CUDA is not available")
+n_devices = torch.cuda.device_count()
+print("\nWelcome!\n\nCUDA devices available:\n")
+for i in range(n_devices):
+    print("\t{}\twith CUDA capability {}".format(torch.cuda.get_device_name(device=i), torch.cuda.get_device_capability(device=i)))
+print("\n")
+device = torch.device("cuda", 0)
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Turn off interactive plotting: for long run it makes screwing up everything
+plt.ioff()
+
+# Here we choose the geometry with 9 time the radiation length
+params = Parameters("4X0")  #!!!!!!!!!!!!!!!!!!!!!CHANGE THE DIMENTION !!!!!!!!!!!!!!!!
+processed_file_path = os.path.expandvars("/dcache/bfys/smitra/DS5/new_ship_tt_processed_data_forcompressed") #!!!!!!!!!!!!!!!!!!!!!CHANGE THE PATH !!!!!!!!!!!!!!!!
+step_size = 5000    # size of a chunk
+#file_size = 180000  # size of the BigFile.root file
+file_size = 120000
+n_steps = int(file_size / step_size) # number of chunks
+
+# ----------------------------------------debug ------------------------------------------------------------------------------------
+#tracemalloc.start()
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------------------ LOAD THE reindex_TT_df & reindex_y_full PD.DATAFRAME --------------------------------------------------------------------------
+
+chunklist_TT_df = []  # list of the TT_df file of each chunk
+chunklist_y_full = [] # list of the y_full file of each chunk
+
+# It is reading and analysing data by chunk instead of all at the time (memory leak problem)
+print("\nReading the tt_cleared_reduced.pkl & y_cleared.pkl files by chunk")
+#First 2 
+outpath = processed_file_path + "/{}".format(0)
+#chunklist_TT_df.append(pd.read_pickle(os.path.join(outpath, "tt_cleared_reduced.pkl")))
+chunklist_TT_df.append(pd.read_pickle(os.path.join(outpath, "tt_cleared.pkl")))
+chunklist_y_full.append(pd.read_pickle(os.path.join(outpath, "y_cleared.pkl")))
+outpath = processed_file_path + "/{}".format(1)
+#chunklist_TT_df.append(pd.read_pickle(os.path.join(outpath, "tt_cleared_reduced.pkl")))
+chunklist_TT_df.append(pd.read_pickle(os.path.join(outpath, "tt_cleared.pkl")))
+chunklist_y_full.append(pd.read_pickle(os.path.join(outpath, "y_cleared.pkl")))
+
+reindex_TT_df = pd.concat([chunklist_TT_df[0],chunklist_TT_df[1]],ignore_index=True)
+
+reindex_y_full = pd.concat([chunklist_y_full[0],chunklist_y_full[1]], ignore_index=True)
+
+pd.set_option('display.max_columns',10)
+
+#print (reindex_TT_df)
+reindex_TT_df.to_csv("reindex_TT_df.csv")
+
+
+for i in tqdm(range(n_steps-2)):  # tqdm: make your loops show a progress bar in terminal
+    outpath = processed_file_path + "/{}".format(i+2)
+    chunklist_TT_df.append(pd.read_pickle(os.path.join(outpath, "tt_cleared.pkl"))) # add all the tt_cleared.pkl files read_pickle and add to the chunklist_TT_df list
+    chunklist_y_full.append(pd.read_pickle(os.path.join(outpath, "y_cleared.pkl"))) # add all the y_cleared.pkl files read_pickle and add to the chunklist_y_full list
+    reindex_TT_df = pd.concat([reindex_TT_df,chunklist_TT_df[i+2]], ignore_index=True)
+    reindex_y_full = pd.concat([reindex_y_full,chunklist_y_full[i+2]], ignore_index=True)
+
+ 
+     
+# reset to empty space
+chunklist_TT_df = []
+chunklist_y_full = []
+#nb_of_plane = len(params.snd_params[params.configuration]["TT_POSITIONS"])
+nb_of_plane = 2
+#----------------------------------------- Ploting figure of the 6 component of TT_df
+'''
+index=1
+response = digitize_signal(reindex_TT_df.iloc[index], params=params, filters=nb_of_plane)
+print("Response shape:",response.shape) # gives  (6, 250, 298) unreduced and (6, 132, 154) for reduced data --> change the network 
+#number_of_paramater = response.shape[0]
+plt.figure(figsize=(18,nb_of_plane))
+for i in range(nb_of_plane):
+    plt.subplot(1,nb_of_plane,i+1)
+    plt.imshow(response[i].astype("uint8") * 255, cmap='gray')
+plt.show()
+'''
+# True value of NRJ/dist for each true electron event
+#y = reindex_y_full[["E", "Z","THETA"]]
+y = reindex_y_full[["E"]]
+NORM = 1. / 100
+y["E"] *= NORM
+#y["Z"] *= -1
+#y["THETA"] *= (180/np.pi)
+
+
+# reset to empty space
+#reindex_y_full = []
+
+# Spliting
+print("\nSplitting the data into a training and a testing sample")
+
+indeces = np.arange(len(reindex_TT_df))
+#print(indeces)
+train_indeces, test_indeces, _, _ = train_test_split(indeces, indeces, train_size=0.9, random_state=1543)
+
+
+
+#print(len(test_indeces))
+
+def indices_by_condition(df, train_indices, column_name, lower_bound, upper_bound):
+
+     # define dataframe containing only test values
+    test_df = df.drop(train_indices)
+
+              # filter out all values where energy is wrong
+    filtered_df = test_df[test_df[column_name] >= lower_bound] 
+    filtered_df = filtered_df[filtered_df[column_name] <= upper_bound] 
+
+    return filtered_df.index.tolist()
+               
+
+final_test_indeces = indices_by_condition(reindex_y_full,train_indeces,'E', 200,400)
+#print(len(final_test_indeces))
+#print(len(train_indeces))
+
+
+#reset to empty space
+reindex_y_full = []
+
+#batch_size = 512
+batch_size = 150
+
+train_dataset = MyDataset(reindex_TT_df, y, params, train_indeces, n_filters=nb_of_plane)
+train_batch_gen = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+#print("TRAIN_BATCH_GEN" , train_batch_gen)
+
+         
+
+test_dataset = MyDataset(reindex_TT_df, y, params, final_test_indeces, n_filters=nb_of_plane)
+test_batch_gen = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+
+# reset to empty space
+reindex_TT_df=[]
+
+# Saving the true Energy for the test sample
+TrueE_test=y["E"][final_test_indeces]
+
+#print("TrueE_test:" ,  TrueE_test)
+#TrueE_test=y["E"][test_indices_smallE_range]
+np.save("TrueE_test_40ep_trial.npy",TrueE_test)
+
+# Creating the network
+net = SNDNet(n_input_filters=nb_of_plane).to(device)
+
+# Loose rate, num epoch and weight decay parameters of our network backprop actions
+lr = 1e-3
+opt = torch.optim.Adam(net.model.parameters(), lr=lr, weight_decay=0.01)
+#num_epochs = 40
+num_epochs = 1
+
+train_loss = []
+val_accuracy_1 = []
+val_accuracy_2 = []
+
+# Create a directory where to store the 9X0 files
+os.system("mkdir 9X0_file")
+
+
+#Training
+print("\nNow Trainig the network:")
+# Create a .txt file where we will store some info for graphs
+f=open("NN_trial_performance.txt","a")
+f.write("Epoch/Time it took (s)/Loss/Validation energy (%)/Validation distance (%)\n")
+f.close()
+
+class Logger(object):
+    def __init__(self):
+        pass
+
+    def plot_losses(self, epoch, num_epochs, start_time):
+        # Print and save in NN_performance.txt the results for this epoch:
+        print("Epoch {} of {} took {:.3f}s".format(epoch + 1, num_epochs, time.time() - start_time))
+        print("  training loss (in-iteration): \t{:.6f}".format(train_loss[-1]))
+        print("  validation Energy:\t\t{:.4f} %".format(val_accuracy_1[-1]))
+        #print("  validation distance:\t\t{:.4f} %".format(val_accuracy_2[-1]))
+
+        f=open("NN_trial_performance.txt","a")
+        f.write("{};{:.3f};".format(epoch + 1, time.time() - start_time))
+        f.write("\t{:.6f};".format(train_loss[-1]))
+        f.write("\t\t{:.4f}\n".format(val_accuracy_1[-1]))
+        #f.write("\t\t{:.4f}\n".format(val_accuracy_2[-1]))
+        f.close()
+
+
+def run_training(lr, num_epochs, opt):
+    try:
+        for epoch in range(num_epochs):
+            # In each epoch, we do a full pass over the training data:
+            start_time = time.time()
+            net.model.train(True)
+            epoch_loss = 0
+            for X_batch, y_batch in tqdm(train_batch_gen, total = int(len(train_indeces) / batch_size)):
+#            for X_batch, y_batch in train_batch_gen:
+            # train on batch
+                
+                loss = net.compute_loss(X_batch, y_batch)
+                #print(loss)
+                loss.backward()
+                opt.step()
+                opt.zero_grad()
+                epoch_loss += loss.item()
+           
+            train_loss.append(epoch_loss / (len(train_indeces) // batch_size + 1))
+
+            y_score = []
+            with torch.no_grad():
+                for (X_batch, y_batch) in tqdm(test_batch_gen, total = int(len(final_test_indeces) / batch_size)):
+                    logits = net.predict(X_batch)
+                    y_pred = logits.cpu().detach().numpy()
+                    y_score.extend(y_pred)
+
+            y_score = mean_squared_error(y.iloc[final_test_indeces], np.asarray(y_score), multioutput='raw_values')
+            val_accuracy_1.append(y_score[0])
+            #val_accuracy_2.append(y_score[1])    
+
+            # Visualize
+            display.clear_output(wait=True)
+            logger.plot_losses(epoch, num_epochs, start_time)
+
+            #Saving network for each 10 epoch
+            if (epoch + 1) % 1 == 0:
+                with open("9X0_file/" + str(epoch) + "_9X0_coordconv.pt", 'wb') as f:
+                    torch.save(net, f)       
+                lr = lr / 2
+                opt = torch.optim.Adam(net.model.parameters(), lr=lr)
+    except KeyboardInterrupt:
+        pass
+
+logger = Logger()
+run_training(lr, num_epochs, opt)
+
+# Saving the prediction at each epoch
+
+# Create a directory where to store the prediction files
+os.system("mkdir PredE_file")
+
+for i in [0]:
+    net = torch.load("9X0_file/" + str(i) + "_9X0_coordconv.pt")
+    preds = []
+    with torch.no_grad():
+        for (X_batch, y_batch) in test_batch_gen:
+            preds.append(net.predict(X_batch))
+    ans = np.concatenate([p.detach().cpu().numpy() for p in preds])
+    np.save("MSE_loss/" + str(i) + "_PredE_test_1ep_trial.npy",ans[:, 0])
+    print("Save Prediction for epoch "+ str(i))
+
+
+#snapshot = tracemalloc.take_snapshot()
+#top_stats = snapshot.statistics('lineno')
+
+#print("[ Top 10 ]")
+#for stat in top_stats[:10]:
+#    print(stat)
+
---
+++ net_copy.py
@@ -0,0 +1,240 @@
+from operator import truediv
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+import numpy as np
+from bisect import bisect_left
+from utils import CM_TO_MUM
+from coord_conv import CoordConv
+import pandas as pd
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, k_size=2, pool=False):
+        super().__init__()
+        self.block = nn.Sequential()
+#        self.block.add_module("conv", CoordConv(in_channels, out_channels,
+#                                                kernel_size=(k_size, k_size), stride=1, with_r=True))
+        self.block.add_module("conv",nn.Conv2d(in_channels, out_channels,
+                                                kernel_size=(k_size, k_size), stride=1, padding=1))
+        if pool:
+            self.block.add_module("Pool", nn.MaxPool2d(2))
+        self.block.add_module("BN", nn.BatchNorm2d(out_channels))
+        self.block.add_module("Act", nn.ReLU())
+        # self.block.add_module("dropout", nn.Dropout(p=0.5))
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class SNDNet(nn.Module):
+    def __init__(self, n_input_filters):
+        super().__init__()
+        self.model = nn.Sequential(
+            Block(n_input_filters, 32, pool=True),
+            Block(32, 32, pool=True),
+            Block(32, 32, pool=True),
+            Block(32, 32, pool=True),
+            #Block(32, 32, pool=False),
+            #Block(128, 128, pool=False),
+            Flatten(),
+            nn.Linear(128, 1),
+            # nn.ReLU(),
+            # nn.Dropout(p=0.5),
+            # nn.Linear(512, 512),
+            # nn.ReLU(),
+            #nn.Linear(1280,2)
+        )
+#        self.model.summary()
+    def compute_loss(self, X_batch, y_batch):
+        X_batch = X_batch.to(self.device)
+        y_batch = y_batch.to(self.device)
+        logits = self.model(X_batch)
+        
+        mse_loss_tensor = F.mse_loss(logits, y_batch, reduction='none')
+        
+        norm_mse_loss_tensor = torch.div(mse_loss_tensor,y_batch)
+        return norm_mse_loss_tensor.mean()
+
+
+
+    def predict(self, X_batch):
+        self.model.eval()
+        return self.model(X_batch.to(self.device))
+
+    @property
+    def device(self):
+   #     for parameters in self.model.parameters():
+    #        print (parameters)
+        return next(self.model.parameters()).device
+
+
+class MyDataset(Dataset):
+    """
+    Class defines how to preprocess data before feeding it into net.
+    """
+    def __init__(self, TT_df, y, parameters, data_frame_indices, n_filters):
+        """
+        :param TT_df: Pandas DataFrame of events
+        :param y: Pandas DataFrame of true electron energy and distance
+        :param parameters: Detector configuration
+        :param data_frame_indices: Indices to train/test on
+        :param n_filters: Number of TargetTrackers in the simulation
+        """
+        self.indices = data_frame_indices
+        self.n_filters = n_filters
+        self.X = TT_df
+        self.y = y
+        self.params = parameters
+
+    def __getitem__(self, index):
+        return torch.FloatTensor(digitize_signal(self.X.iloc[self.indices[index]],
+                                                 self.params,
+                                                 filters=self.n_filters)),\
+               torch.FloatTensor(self.y.iloc[self.indices[index]])
+
+    def __len__(self):
+        return len(self.indices)
+
+
+def compress_digitize_signal(event, params, filters=1):
+    """
+    Convert pandas DataFrame to image
+    :param event: Pandas DataFrame line
+    :param params: Detector configuration
+    :param filters: Number of TargetTrackers in the simulation
+    :return: numpy tensor of shape (n_filters, H, W).
+    """    
+    
+    shape = (filters, 
+             int(np.ceil(params.snd_params["X_HALF_SIZE"] * 2 * CM_TO_MUM /
+                         params.snd_params["RESOLUTION"])),
+             int(np.ceil(params.snd_params["X_HALF_SIZE"] * 2 * CM_TO_MUM /
+                         params.snd_params["RESOLUTION"])))
+    response = np.zeros(shape)
+    '''
+    pixels_H = int(np.ceil(params.snd_params["X_HALF_SIZE"] * 2 * CM_TO_MUM /
+                           params.snd_params["RESOLUTION"]))
+    pixels_W = int(np.ceil(params.snd_params["X_HALF_SIZE"] * 2 * CM_TO_MUM /
+                         params.snd_params["RESOLUTION"]))
+
+    print(pixels_H)
+    shape = (filters, pixels_H, pixels_W)
+
+    response = np.zeros(shape)
+
+    for i in range(len(event['Z'])):
+        if np.logical_and(event['Z'][i] >= -3041.0, event['Z'][i]<= -3037.0):
+            event['Z'][i] = -3020.0
+        elif np.logical_and(event['Z'][i] >= -3032.0, event['Z'][i]<= -3027.0):
+            event['Z'][i] = -3015.0
+        elif np.logical_and(event['Z'][i] >= -3022.0, event['Z'][i] <= -3017.0):
+            event['Z'][i] = -3010.0
+        elif np.logical_and(event['Z'][i] >= -3012.0, event['Z'][i] <= -3007.0):
+            event['Z'][i] = -3005.0
+        else:
+            pass
+    
+    min_z_value = -3020.0
+    event['Z'] = event['Z'] - min_z_value
+    max_Z_val = np.max(event['Z'])
+    event['Z'] = (event['Z']/max_Z_val)*(int(np.ceil(params.snd_params["X_HALF_SIZE"] * 2 * CM_TO_MUM /
+                         params.snd_params["RESOLUTION"]))-1)
+    print(event['Z'])
+    for x_index,y_index, z_pos in zip(np.floor((event['X'] + params.snd_params["X_HALF_SIZE"]) * CM_TO_MUM /
+                                       params.snd_params["RESOLUTION"]).astype(int),
+                                      np.floor((event['Y'] + params.snd_params["X_HALF_SIZE"]) * CM_TO_MUM /
+                                       params.snd_params["RESOLUTION"]).astype(int),
+                                      np.floor( event['Z']).astype(int)):
+
+ 
+        response[0,z_pos,x_index] += 1
+        response[1,z_pos,shape[1] - y_index - 1]+=1
+    print(response[0])
+    return response
+    '''
+    for x_index, y_index, z_pos in zip(np.floor((event['X'] + params.snd_params["X_HALF_SIZE"]) * CM_TO_MUM /
+                                                params.snd_params["RESOLUTION"]).astype(int),
+                                       np.floor((event['Y'] + params.snd_params["Y_HALF_SIZE"]) * CM_TO_MUM /
+                                                params.snd_params["RESOLUTION"]).astype(int),
+                                       event['Z']):
+        response[params.tt_map[bisect_left(params.tt_positions_ravel, z_pos)],
+                 shape[1] - y_index - 1,
+                 x_index] += 1                
+           
+    return response
+
+def digitize_signal(event, params, filters=2):
+
+    shape = (filters,4,int(np.ceil(params.snd_params["X_HALF_SIZE"] * 2 * CM_TO_MUM /
+                                   params.snd_params["RESOLUTION"])))
+
+    response = np.zeros(shape)
+
+    first_layer_x = []
+    first_layer_y = []
+    second_layer_x = []
+    second_layer_y = []
+    third_layer_x = []
+    third_layer_y = []
+    fourth_layer_x = []
+    fourth_layer_y = []
+
+    for i in range(len(event['Z'])):
+        if np.logical_and(event['Z'][i] >= -3041.0, event['Z'][i]<= -3037.0):
+            first_layer_x.append(event['X'][i])
+            first_layer_y.append(event['Y'][i])
+        elif np.logical_and(event['Z'][i] >= -3032.0, event['Z'][i]<= -3027.0):
+            second_layer_x.append(event['X'][i])
+            second_layer_y.append(event['Y'][i])
+        elif np.logical_and(event['Z'][i] >= -3022.0, event['Z'][i] <= -3017.0):
+            third_layer_x.append(event['X'][i])
+            third_layer_y.append(event['Y'][i])
+        elif np.logical_and(event['Z'][i] >= -3012.0, event['Z'][i] <= -3007.0):
+            fourth_layer_x.append(event['X'][i])
+            fourth_layer_y.append(event['Y'][i])
+        else:
+            pass
+    event_comp_x = pd.DataFrame({'z1': pd.Series(first_layer_x), 'z2': pd.Series(second_layer_x), 'z3': pd.Series(third_layer_x), 'z4': pd.Series(fourth_layer_x) })
+    event_comp_x.fillna(0, inplace=True)
+    event_comp_y = pd.DataFrame({'z1': pd.Series(first_layer_y), 'z2': pd.Series(second_layer_y), 'z3': pd.Series(third_layer_y), 'z4': pd.Series(fourth_layer_y) })
+    event_comp_y.fillna(0, inplace=True)
+
+    
+    for x_one, x_two, x_three, x_four in zip(np.floor((event_comp_x['z1'] + params.snd_params["X_HALF_SIZE"])*CM_TO_MUM/
+                                                       params.snd_params["RESOLUTION"]).astype(int), 
+                                             np.floor((event_comp_x['z2'] + params.snd_params["X_HALF_SIZE"])*CM_TO_MUM/
+                                                       params.snd_params["RESOLUTION"]).astype(int), 
+                                             np.floor((event_comp_x['z3'] + params.snd_params["X_HALF_SIZE"])*CM_TO_MUM/
+                                                       params.snd_params["RESOLUTION"]).astype(int),
+                                             np.floor((event_comp_x['z4'] + params.snd_params["X_HALF_SIZE"])*CM_TO_MUM/
+                                                       params.snd_params["RESOLUTION"]).astype(int)):
+#        print(x_one)
+#        print(x_two)
+#        print(x_three)
+#        print(x_four)
+        response[0,0,x_one] +=1
+        response[0,1, x_two] += 1
+        response[0,2,x_three] +=1
+        response[0,3,x_four] += 1
+
+    for y_one, y_two, y_three, y_four in zip (np.floor((event_comp_y['z1'] + params.snd_params["Y_HALF_SIZE"])*CM_TO_MUM/
+                                                        params.snd_params["RESOLUTION"]).astype(int),
+                                              np.floor((event_comp_y['z2'] + params.snd_params["Y_HALF_SIZE"])*CM_TO_MUM/
+                                                        params.snd_params["RESOLUTION"]).astype(int),
+                                              np.floor((event_comp_y['z3'] + params.snd_params["Y_HALF_SIZE"])*CM_TO_MUM/
+                                                        params.snd_params["RESOLUTION"]).astype(int),
+                                              np.floor((event_comp_y['z4'] + params.snd_params["Y_HALF_SIZE"])*CM_TO_MUM/
+                                                        params.snd_params["RESOLUTION"]).astype(int)):
+        response[1,0,shape[1] - y_one - 1] += 1
+        response[1,1,shape[1] - y_two - 1] += 1
+        response[1,2,shape[1] - y_three - 1] += 1
+        response[1,3,shape[1] - y_four - 1] += 1
+    response = (response>=1).astype(int)    
+#    print(response)
+    return response    
